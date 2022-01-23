c28de9467144f9fb263c9f3380f72eadc9d3e49e
Tim <43130816+DerStimmler@users.noreply.github.com>
2022-01-23 14:27:56 +0100

    feat(primeng): install primeng

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PrimeNGConfig } from 'primeng/api';
 
 @NgModule({
   imports: [CommonModule]
 })
-export class UiModule {}
+export class UiModule {
+  constructor(private primengConfig: PrimeNGConfig) {
+    this.primengConfig.ripple = false;
+  }
+}
