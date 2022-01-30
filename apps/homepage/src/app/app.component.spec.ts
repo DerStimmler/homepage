7876befbb1480aaa7594d515a02469e539c6c826
Tim <43130816+DerStimmler@users.noreply.github.com>
2022-01-30 22:02:30 +0100

    chore: fix lint issues

--- apps/homepage/src/app/app.component.spec.ts
+++ apps/homepage/src/app/app.component.spec.ts
@@ -7,28 +7,30 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent, NxWelcomeComponent],
+      declarations: [AppComponent, NxWelcomeComponent]
     }).compileComponents();
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'homepage'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app.title).toEqual('homepage');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
+
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome homepage'
-    );
+
+    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome homepage');
   });
 });
